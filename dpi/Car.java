14c02e6312f4bc1fdfe577ea7f992e3cdc01636a
Farid Rizal <faridrizal1111@gmail.com>
2023-10-25 11:24:06 +0700

    perubahan dpi

--- dpi/Car.java
+++ dpi/Car.java
@@ -2,9 +2,9 @@ package dpi;
 
 public class Car {
 
-  private final Engine engine;
+  private final EngineInterface engine;
 
-  public Car(Engine engine) {
+  public Car(EngineInterface engine) {
     this.engine = engine;
   }
 
