8c588dac96bf31d0823b517b5f317a0a0d1195b3
Farid Rizal <faridrizal1111@gmail.com>
2023-11-20 15:12:17 +0700

    Perubahan kode  program Customer.java(Use Pronounceable Names

--- cleancode/Customer.java
+++ cleancode/Customer.java
@@ -1,8 +1,8 @@
 import java.sql.Date;
 
 class Customer {
-  private Date genymdhms;
-  private Date modymdhms;
-  private final String pszqint = "102";
+  private Date generationTimestamp;
+  private Date modificationTimestamp;
+  private final String recordId = "102";
   /* ... */
 }
