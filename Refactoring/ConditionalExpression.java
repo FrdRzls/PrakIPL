8f01b138e61a2af5a3ff51314e2b0636a371d0f0
Farid Rizal <faridrizal1111@gmail.com>
2023-12-25 13:24:31 +0700

    Menyederhanakan Kondisi If sebelum dilakukan refactoring

---
+++ Refactoring/ConditionalExpression.java
@@ -0,0 +1,13 @@
+public class ConditionalExpression {
+//Sebelum Reafctoring
+if (score > 700)
+    accept();
+else if ((income >= 40000) && (income <= 100000)
+        && authorized && (score > 500))
+accept();
+else if (income > 100000)
+    accept();
+else
+    reject();
+
+}
